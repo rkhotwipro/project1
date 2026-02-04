8656182af5310fe81e424bdf95e6373bca0bf460
rkhotwipro <rahul.khot@wipro.com>
2026-02-04 10:30:40 +0530

    msg

---
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,15 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementAppTest {
+
+    public static class Foot{
+        private final double value1;
+        private final double value2;
+
+
+        public Foot(double value1, double value2) {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+    }
+}
